966c9a98cba70e7b399aecd82a91e8ef65370309
TonyAraujo88 <tonybugattieb@gmail.com>
2024-07-15 00:44:18 -0300

    dados js

--- JavaScript/desafio_sentenca_bloco.js
+++ JavaScript/desafio_sentenca_bloco.js
@@ -1,15 +1,15 @@
 {
-    console.log("Bloco #01");
-    console.log("Bloco #03");
-    console.log("Bloco #05");
-    console.log("Bloco #07");
-    console.log("Bloco #09");
+    console.log("1");
+    console.log("3");
+    console.log("5");
+    console.log("7");
+    console.log("9");
 }
 
 {
-    console.log("Bloco #02");
-    console.log("Bloco #04");
-    console.log("Bloco #06");
-    console.log("Bloco #08");
-    console.log("Bloco #010");   
+    console.log("2");
+    console.log("4");
+    console.log("6");
+    console.log("8");
+    console.log("10");   
 }
