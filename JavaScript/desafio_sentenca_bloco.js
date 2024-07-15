c961e6209d816c5e4f7abe63619496dbc9fbe3db
TonyAraujo88 <tonybugattieb@gmail.com>
2024-07-14 23:06:32 -0300

    Cod3r js

---
+++ JavaScript/desafio_sentenca_bloco.js
@@ -0,0 +1,15 @@
+{
+    console.log("Bloco #01");
+    console.log("Bloco #03");
+    console.log("Bloco #05");
+    console.log("Bloco #07");
+    console.log("Bloco #09");
+}
+
+{
+    console.log("Bloco #02");
+    console.log("Bloco #04");
+    console.log("Bloco #06");
+    console.log("Bloco #08");
+    console.log("Bloco #010");   
+}
